e5ded60bd6fb14fbfa825a1f768f701e68c492eb
Agustin Sanchez <trorik23@gmail.com>
2019-05-27 17:58:37 -0300

    ejemplo funcionando master-slave

--- trabajo final/workload.c
+++ trabajo final/workload.c
@@ -1,4 +1,6 @@
 #include <mpi.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #define WORK_TAG 1
 #define FINISH_TAG 2
@@ -26,20 +28,22 @@ void master()
 	double result;
 	MPI_Status status;
 	MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
-
+	
+	work = 0;
 	for (rank = 1; rank < ntasks; ++rank) //tarea inicial
 	{
 		// work = nuevo trabajo para cada slave;
 		MPI_Send(&work,1,MPI_INT,rank,WORK_TAG,MPI_COMM_WORLD);
 	}
-
-	while (/* haya mas trabajo */)
+	work = 1;
+	while (work < 10/* haya mas trabajo */)
 	{
 		//recibir resultados
 		MPI_Recv(&result,1,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 		//mandarle mas trabajo
 		// work = nuevo trabajo para cada slave;       
 		MPI_Send(&work, 1, MPI_INT, status.MPI_SOURCE,WORK_TAG, MPI_COMM_WORLD);
+		work++;
 	}
 
 	//recibir trabajo de los restantes
@@ -66,9 +70,35 @@ void slave()
 
 		if (status.MPI_TAG == FINISH_TAG)
 			return;
-
-		/* do the work */
+		
+		/* Start task */
+		printf("Soy %d ejecutando el task %d\n", rank, work);
+		usleep(rank*0.01); //simulation
+		/* End task */
 
 		MPI_Send(&result, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 	}
 }
+/*
+mpirun -np 6 run.out
+cada slave con diferentes tiempos de ejecucion
+ 
+Soy 1 ejecutando el task 0
+Soy 1 ejecutando el task 2
+Soy 1 ejecutando el task 5
+Soy 1 ejecutando el task 8
+
+Soy 2 ejecutando el task 0
+Soy 2 ejecutando el task 1
+Soy 2 ejecutando el task 6
+
+Soy 3 ejecutando el task 0
+Soy 3 ejecutando el task 3
+Soy 3 ejecutando el task 7
+Soy 3 ejecutando el task 9
+
+Soy 4 ejecutando el task 0
+
+Soy 5 ejecutando el task 0
+Soy 5 ejecutando el task 4
+*/
