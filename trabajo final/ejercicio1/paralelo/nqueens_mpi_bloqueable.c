3419165908f96f8a0ebad51588382198f5a1f07e
Agustin Sanchez <trorik23@gmail.com>
2019-06-07 13:17:27 -0300

    valores correctos

--- trabajo final/ejercicio1/paralelo/nqueens_mpi_bloqueable.c
+++ trabajo final/ejercicio1/paralelo/nqueens_mpi_bloqueable.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
+#include "valores_correctos.h"
 
 #define WORK_TAG 1
 #define FINISH_TAG 2
@@ -100,32 +101,6 @@ int main(int argc, char *argv[])
 	N = atoi(argv[1]);
 	queens = malloc(sizeof(int) * N);
 
-	if (rank == 0)
-		printf("\nCalculando para N=%d y np=%d\n", N, P);
-
-	//calcular workload, osea profundidad del arbol a pasar
-	//TODO: lo hacen todos. esta mal?
-	num_col = 1;
-	int workload = N;
-	int previous_workload = 0;
-	while (workload < P && num_col <= N)
-	{
-		workload = 0;
-		num_col++;
-		recursive_queens_truncado(0, queens, &workload, (num_col - 1));
-		if (rank == 0)
-			printf("C=%d y work=%d\n", num_col, workload);
-		if (workload <= previous_workload)
-		{ // no tiene sentido seguir iterando
-			workload = previous_workload;
-			num_col--;
-			break;
-		}
-		previous_workload = workload;
-	}
-	//TODO: que pasa si llega a N
-	if (rank == 0)
-		printf("Quedo en C=%d y work=%d\n", num_col, workload);
 	if (rank == 0)
 		master();
 	else
@@ -232,17 +207,11 @@ void recursive_queens_master(int index, int *queens, int *total_solutions, int *
 	static int unread_msg;
 	if (!finished)
 	{
-
 		MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &unread_msg, &status);
 		if (unread_msg)
 		{
-			//printf("Me intenta frenar %d\n", status.MPI_SOURCE);
 			if (next_work_recursive(q_workload, num_col - 1))
 			{
-				//for (int x = 0; x < num_col; x++)
-				//	printf("\t%d ", q_workload[x]);
-				//printf("\n");
-				//printf("Hay mas trabajo %d\n", status.MPI_SOURCE);
 				//recibir solicitudes
 				MPI_Recv(0, 0, MPI_INT, MPI_ANY_SOURCE, WORK_TAG, MPI_COMM_WORLD, &status);
 				//mandarle mas trabajo
@@ -277,19 +246,45 @@ void recursive_queens_master(int index, int *queens, int *total_solutions, int *
 	}
 }
 
+void calculate_workload_depth()
+{
+	//calcular workload, osea profundidad del arbol a pasar
+	num_col = 1;
+	int workload = N;
+	int previous_workload = 0;
+	while (workload < P && num_col <= N)
+	{
+		workload = 0;
+		num_col++;
+		recursive_queens_truncado(0, queens, &workload, (num_col - 1));
+		if (rank == 0)
+			printf("C=%d y work=%d\n", num_col, workload);
+		if (workload <= previous_workload)
+		{ // no tiene sentido seguir iterando
+			workload = previous_workload;
+			num_col--;
+			break;
+		}
+		previous_workload = workload;
+	}
+	if (rank == 0)
+		printf("Quedo en C=%d y work=%d\n", num_col, workload);
+}
+
 void master()
 {
-	//sleep(1);//darle tiempo a los procesos que inicien.
-	double timetick = dwalltime();
-	int total = 0;
-	int *q_workload;
+	if (rank == 0)
+		printf("\nCalculando para N=%d y np=%d\n", N, P);
 
-	q_workload = malloc(sizeof(int) * num_col);
+	double timetick = dwalltime();
+
+	calculate_workload_depth();	
+
+	int *q_workload = malloc(sizeof(int) * num_col);
 	for (int x = 0; x < num_col; x++)
 		q_workload[x] = 0;
 
 	int unread_msg;
-
 	while (next_work_recursive(q_workload, num_col - 1))
 	{
 		//for (int x = 0; x < num_col; x++)
@@ -318,23 +313,29 @@ void master()
 	for (int r = 1; r < P; r++)
 		MPI_Send(0, 0, MPI_INT, r, FINISH_TAG, MPI_COMM_WORLD);
 	//junto sus resultados
+	unsigned long int total = 0;
 	MPI_Reduce(&result, &total, 1, MPI_INT, MPI_SUM, MASTER_RANK, MPI_COMM_WORLD);
 
-	printf("Soluciones = %d\t| Tiempo = %.4f\n\n", total, dwalltime() - timetick);
+	printf("Soluciones = %lu\t| Tiempo = %.4f\n", total, dwalltime() - timetick);
+	if(valores_correctos[N] == total)
+		printf("Solucion correcta.\n");
+	else
+		printf("Solucion incorrecta.\n");
+	printf("\n");
 	free(q_workload);
 }
 
 void slave()
 {
-
 	for (;;)
 	{
 		MPI_Send(0, 0, MPI_INT, MASTER_RANK, WORK_TAG, MPI_COMM_WORLD); //ask for work
-		MPI_Recv(queens, num_col, MPI_INT, MASTER_RANK, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		MPI_Recv(queens, N, MPI_INT, MASTER_RANK, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		if (status.MPI_TAG == FINISH_TAG)
 			break;
 		/* Start task */
 		iteraciones++;
+		MPI_Get_count(&status, MPI_INT, &num_col);
 		recursive_queens(num_col, queens, &result);
 	}
 	//Devolver mi trabajo
---
+++ trabajo final/ejercicio1/paralelo/valores_correctos.h
@@ -0,0 +1,26 @@
+#ifndef VALORES_CORRECTOS
+#define VALORES_CORRECTOS
+unsigned long int valores_correctos[] = {
+    0,
+    1,
+    0,
+    0,
+    2,
+    10,
+    4,
+    40,
+    92,
+    352,
+    724,
+    2680,
+    14200,
+    73712,
+    365596,
+    2279184,
+    14772512,
+    95815104,
+    666090624,
+    4968057848,
+    39029188884};
+
+#endif
