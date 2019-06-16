8dd3aedca660b93abbae3c6577df0decd73b5f96
Agustin Sanchez <trorik23@gmail.com>
2019-06-16 14:07:27 -0300

    queens secuencial en dos modos

--- trabajo final/ejercicio2/secuencial/nqueens_secuencial.c
+++ trabajo final/ejercicio2/secuencial/nqueens_secuencial.c
@@ -9,6 +9,7 @@ posibles soluciones para un tablero de tamaño NxN.
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "valores_correctos.h"
 
 unsigned int N; //Tamanio vector
 unsigned int *queens;
@@ -64,17 +65,79 @@ void recursive_queens(int index)
 		}
 	}
 }
+void get_queens()
+{
+	int check;
+	int j;
+	int col = 0;
+	queens[0] = 0;
+	while (1)
+	{
+		j = 0;
+		check = 1;
+		while (j < col && check)
+		{
+			if ((queens[j] == queens[col]) || (queens[j] == queens[col] - (j - col)) || (queens[j] == queens[col] + (j - col)))
+				check = 0;
+			j++;
+		}
+		if (check)
+		{
+			if (col == N-1) // Era la ultima columna
+				total_solutions++;
+			else // Sigo buscando
+			{
+				col++;
+				queens[col] = 0;
+				continue;
+			}
+		}
+		//update index
+		queens[col]++;
+		if (queens[col] == N) //termino de analizar esta columna
+		{
+			if (col == 0) //si era donde arranco, termine
+				return;
+			else
+			{
+				col--;
+				queens[col]++;
+				if (queens[col] == N)
+				{
+					if (col == 0)
+						return;
+					else
+					{
+						col--;
+						queens[col]++;
+					}
+				}
+			}
+		}
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	double timetick;
 	N = atoi(argv[1]);
 	queens = malloc(sizeof(int) * N);
 
-	double timetick = dwalltime();
-
-	recursive_queens(0);
-
+	printf("Solucion recursiva\n");
+	timetick = dwalltime();
+	recursive_queens(0);	
 	printf("N = %d\t| Soluciones = %d\t| Tiempo = %.4f\n", N, total_solutions, dwalltime() - timetick);
+	if (valores_correctos[N] != total_solutions)
+		printf("Solucion incorrecta.\nX\nX\nX\nX\nX\n");
 
+	printf("Solucion no-recursiva\n");
+	total_solutions = 0;
+	timetick = dwalltime();
+	get_queens();
+	printf("N = %d\t| Soluciones = %d\t| Tiempo = %.4f\n", N, total_solutions, dwalltime() - timetick);
+	if (valores_correctos[N] != total_solutions)
+		printf("Solucion incorrecta.\nX\nX\nX\nX\nX\n");
+	
+	free(queens);
 	return 0;
 }
