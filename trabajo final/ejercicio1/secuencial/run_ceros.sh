b96a0bfcdd0cfcff45861fa227808656813c7755
Agustin Sanchez <trorik23@gmail.com>
2019-06-20 20:13:35 -0300

    format test

---
+++ trabajo final/ejercicio1/secuencial/run_ceros.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc secuencial_triangular_ceros.c -o secuencial_triangular_ceros
+./secuencial_triangular_ceros 512
+./secuencial_triangular_ceros 1024
+./secuencial_triangular_ceros 2048
